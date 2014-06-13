1be1c0afc13e54a86ceb9b8806217f93f4384e20
ananth <ur.pal.ananth@gmail.com>
2014-06-13 16:19:47 +0530

    fff

--- WeeklyDashboard/src/com/pega/wd/Test.java
+++ WeeklyDashboard/src/com/pega/wd/Test.java
@@ -4,6 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Testing has been done");
- System.out.println("what is this?");
+		System.out.println("what is this?");
 	}
 }
