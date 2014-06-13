91babb8adae434e5a2d9fdc17ed0786b19daa627
ananth <ur.pal.ananth@gmail.com>
2014-06-13 14:50:55 +0530

    test

--- WeeklyDashboard/src/com/pega/wd/Test.java
+++ WeeklyDashboard/src/com/pega/wd/Test.java
@@ -3,11 +3,6 @@ package com.pega.wd;
 public class Test {
 
 	public static void main(String[] args) {
-
-//		Changes from Ananth
-		// Why are you doing this?
-// 		EDIT:: Kunal
-		// Test .. Eclipse works good.
+		System.out.println("Testing");
 	}
-
 }
