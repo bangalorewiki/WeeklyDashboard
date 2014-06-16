6caef47e88084369455478dc4f1296b2137a1170
kunal <kunalsunny@gmail.com>
2014-06-16 17:42:35 +0530

    created csv object to process.

--- WeeklyDashboard/src/com/pega/wd/ReadCSVImpl.java
+++ WeeklyDashboard/src/com/pega/wd/ReadCSVImpl.java
@@ -1,12 +1,23 @@
 package com.pega.wd;
-
+//changes :: kunal
 import java.io.File;
+import com.csvreader.CsvReader; //CSV library 
 import java.util.ArrayList;
 
 public class ReadCSVImpl extends ReadCSV {
 
 	@Override
 	Object readFile(File file) {
+		
+		try{
+		
+		CsvReader csv=new CsvReader(file);
+		
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}	
 		return null;
 	}
 	
